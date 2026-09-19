25e0c55c2183acbfecfa7ef52db6567b648ba83c
nijass1488-create <@.com>
2026-09-19 12:18:12 +0300

    Add 35+ bytecode heuristics for Python pseudocode generation - Import handling (import, from...import) - Name operations (load, store, delete) - Attribute operations (obj.attr access) - Function calls - Comparisons (==, !=, <, <=, >, >=, is, in) - Binary operations (+, -, *, /, %, &, |, ^, //) - In-place operations (+=, -=, etc.) - Control flow (if, for, jump) - Function creation (make_function)

--- pyc-decompiler/src/bin/main.rs
+++ pyc-decompiler/src/bin/main.rs
@@ -35,7 +35,7 @@ fn main() {
 
     println!("\n=== Strings Found ===");
     for s in report.suspicious_strings.iter().take(15) {
-        println!("  {}", s);
+        println!("  \"{}\"", s);
     }
 
     println!("\n=== Imports ===");
@@ -58,75 +58,211 @@ fn main() {
 fn generate_pseudocode(insts: &[Instruction], consts: &[String]) {
     let mut i = 0;
     let mut indent = 0;
-    let mut line_count = 0;
 
-    while i < insts.len() && line_count < 100 {
-        line_count += 1;
+    while i < insts.len() {
         let inst = &insts[i];
         let indent_str = "  ".repeat(indent);
 
         // Skip common stack manipulation ops
-        if matches!(inst.opcode, Opcode::PopTop | Opcode::RotTwo | Opcode::RotThree) {
-            i += 1;
-            continue;
-        }
-
         match inst.opcode {
+            Opcode::RotTwo | Opcode::RotThree | Opcode::PopTop | Opcode::DupTop | Opcode::DupTopTwo => {
+                i += 1;
+                continue;
+            }
             Opcode::ForIter => {
-                println!("{}for ... in iterable:", indent_str);
+                println!("{}for _ in iterable:", indent_str);
                 indent += 1;
                 i += 1;
                 continue;
             }
-            Opcode::PopTop => {
+            Opcode::ImportName => {
+                let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
+                let module = consts.get(idx).cloned().unwrap_or("?".to_string());
+                println!("{}import {}", indent_str, module);
+                i += 1;
+                continue;
+            }
+            Opcode::ImportFrom => {
+                let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
+                let module = consts.get(idx).cloned().unwrap_or("?".to_string());
+                println!("{}from {} import *", indent_str, module);
                 i += 1;
                 continue;
             }
             Opcode::LoadName => {
                 let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
-                let name = consts.get(idx).cloned().unwrap_or("?".to_string());
-                println!("{}# load {}", indent_str, name);
+                let name = consts.get(idx).cloned().unwrap_or(format!("v{}", idx));
+                println!("{}# load: {}", indent_str, name);
+                i += 1;
+                continue;
             }
             Opcode::StoreName => {
                 let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
-                let name = consts.get(idx).cloned().unwrap_or("?".to_string());
+                let name = consts.get(idx).cloned().unwrap_or(format!("v{}", idx));
                 println!("{}{} = ...", indent_str, name);
+                i += 1;
+                continue;
             }
-            Opcode::CallFunction => {
-                let nargs = inst.arg.map(|a| a / 2).unwrap_or(0);
-                println!("{}# call({})", indent_str, nargs);
+            Opcode::DeleteName => {
+                let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
+                println!("{}# del: {}", indent_str, consts.get(idx).unwrap_or(&"?".to_string()));
+                i += 1;
+                continue;
             }
-            Opcode::CompareOp => {
-                let cmp = match inst.arg.unwrap_or(0) {
-                    0 => "==", 1 => "!=", 2 => "<", 3 => "<=", 4 => ">", 5 => ">=",
-                    6 => "is", 7 => "is not", 8 => "in", 9 => "not in", _ => "?",
-                };
-                println!("{}# cmp {}", indent_str, cmp);
-            }
-            Opcode::JumpIfTrueOrPop | Opcode::JumpIfFalseOrPop => {
-                println!("{}if ...:", indent_str);
-                indent += 1;
+            Opcode::LoadAttr => {
+                let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
+                let name = consts.get(idx).cloned().unwrap_or("?".to_string());
+                println!("{}# obj.{}", indent_str, name);
+                i += 1;
+                continue;
             }
             Opcode::StoreAttr => {
                 let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
                 let name = consts.get(idx).cloned().unwrap_or("?".to_string());
                 println!("{}# obj.{} = ...", indent_str, name);
+                i += 1;
+                continue;
+            }
+            Opcode::DeleteAttr => {
+                let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
+                println!("{}# del obj.{}", indent_str, consts.get(idx).unwrap_or(&"?".to_string()));
+                i += 1;
+                continue;
+            }
+            Opcode::CallFunction => {
+                let nargs = inst.arg.map(|a| a as usize).unwrap_or(0);
+                println!("{}# call({})", indent_str, nargs);
+                i += 1;
+                continue;
+            }
+            Opcode::CompareOp => {
+                let cmp = match inst.arg.unwrap_or(0) {
+                    0 => "==", 1 => "!=", 2 => "<", 3 => "<=", 4 => ">", 5 => ">=",
+                    6 => "is", 7 => "is not", 8 => "in", 9 => "not in", 10 => "issubclass",
+                    11 => "isinstance", _ => "?",
+                };
+                println!("{}# cmp: {}", indent_str, cmp);
+                i += 1;
+                continue;
+            }
+            Opcode::JumpForward => {
+                println!("{}# jump_forward: {}", indent_str, inst.arg.unwrap_or(0));
+                i += 1;
+                continue;
+            }
+            Opcode::JumpIfTrueOrPop | Opcode::JumpIfFalseOrPop => {
+                println!("{}if ...:", indent_str);
+                indent += 1;
+                i += 1;
+                continue;
+            }
+            Opcode::IterNext => {
+                println!("{}# next", indent_str);
+                i += 1;
+                continue;
             }
             Opcode::LoadConst => {
                 let idx = inst.arg.map(|a| a as usize).unwrap_or(0);
-                let val = consts.get(idx).cloned().unwrap_or("?".to_string());
-                println!("{}# const: {}", indent_str, val);
+                if idx < consts.len() {
+                    let val = &consts[idx];
+                    println!("{}# const: {}", indent_str, val);
+                } else {
+                    println!("{}# const: ?", indent_str);
+                }
+                i += 1;
+                continue;
             }
-            Opcode::BinaryAdd => println!("{}# +", indent_str),
-            Opcode::BinarySubtract => println!("{}# -", indent_str),
-            Opcode::BinaryMultiply => println!("{}# *", indent_str),
-            Opcode::BinaryModulo => println!("{}# %", indent_str),
-            Opcode::BinaryAnd => println!("{}# &", indent_str),
-            Opcode::BinaryOr => println!("{}# |", indent_str),
+            // Binary operations
+            Opcode::BinaryAdd => {
+                println!("{}# +", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::BinarySubtract => {
+                println!("{}# -", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::BinaryMultiply => {
+                println!("{}# *", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::BinaryModulo => {
+                println!("{}# %", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::BinaryAnd => {
+                println!("{}# &", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::BinaryOr => {
+                println!("{}# |", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::BinaryXor => {
+                println!("{}# ^", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::BinaryFloorDivide => {
+                println!("{}# //", indent_str);
+                i += 1;
+                continue;
+            }
+            // In-place operations
+            Opcode::InplaceAdd | Opcode::InplaceSubtract | Opcode::InplaceMultiply |
+            Opcode::InplaceModulo | Opcode::InplacePower | Opcode::InplaceAnd |
+            Opcode::InplaceOr | Opcode::InplaceXor | Opcode::InplaceFloorDivide => {
+                println!("{}# op=", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::MakeFunction => {
+                let nargs = inst.arg.map(|a| a as usize).unwrap_or(0);
+                println!("{}# make_function({})", indent_str, nargs);
+                i += 1;
+                continue;
+            }
+            Opcode::CallFunctionEx => {
+                let nargs = inst.arg.map(|a| a as usize).unwrap_or(0);
+                println!("{}# call_ex({})", indent_str, nargs);
+                i += 1;
+                continue;
+            }
+            Opcode::LoadMap => {
+                println!("{}# load_map", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::StoreSubst => {
+                println!("{}# store_subst", indent_str);
+                i += 1;
+                continue;
+            }
+            Opcode::UnpackSequence => {
+                let count = inst.arg.map(|a| a as usize).unwrap_or(0);
+                println!("{}# unpack {} items", indent_str, count);
+                i += 1;
+                continue;
+            }
+            Opcode::UnpackEx => {
+                let count = inst.arg.map(|a| a as usize).unwrap_or(0);
+                println!("{}# unpack_ex({})", indent_str, count);
+                i += 1;
+                continue;
+            }
+            Opcode::StopCode | Opcode::ImportAll | Opcode::PopExcept | Opcode::PopBlock => {
+                i += 1;
+                continue;
+            }
             _ => {
                 let opcode = format!("{:?}", inst.opcode);
                 let arg = inst.arg.map(|a| a.to_string()).unwrap_or("?".to_string());
-                println!("{}# {:15} arg={}", indent_str, opcode, arg);
+                println!("{}# {:20} arg={}", indent_str, opcode, arg);
             }
         }
 
